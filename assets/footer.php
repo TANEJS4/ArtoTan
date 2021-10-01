46cf5435049e743e88bde050dcee0a8e3e93430d
Shivam <35265364+TANEJS4@users.noreply.github.com>
2021-09-30 22:10:34 -0400

    check desc
    * added onclick function (does not search for anything)
    * added search.php that shows the result
     (w/ ss of maps and table of 2)

--- assets/footer.php
+++ assets/footer.php
@@ -1,9 +1,7 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="en">
-<head>
-	<title>Home page – My Website</title>
 	<head>
-		<!-- Mobile Specific Metas -->
+    		<!-- Mobile Specific Metas -->
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=0.9"/>
 
 		<!-- CSS - Bootstrap -->
@@ -11,18 +9,26 @@
 		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 		<!-- favicon -->
 	</head>
-	<body >
+
+	<body>
 		<!-- js -->	
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-			
-		<div style="background-color:#202428">
-			<!--  header file -->
-			<?php require("assets\header.php"); ?>
-			
-			<!--  footer file -->
-			<?php require("assets\\footer.php"); ?>
-		</div>
+		
+    <!-- footer -->
+    <footer class="navbar navbar-dark bg-dark fixed-bottom">
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">foo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">bar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </footer>
 	</body>
-</html
+</html>
