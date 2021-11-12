f41b6f86d6e0362356d40fdefc6544bb48b28842
Artem Kokhanov <artemkokhanov98@gmail.com>
2021-11-12 14:18:52 -0500

    adding Artem's changes

---
+++ assets/registration.js
@@ -0,0 +1,31 @@
+document.getElementById("login").onclick = function () {
+    location.href = "./registration.html";
+};
+
+// Registration JS - Form Validation:
+// grabbing DOM elements
+const regForm = document.querySelector('#regForm');
+const regFOrmErrorMsg = document.querySelector('.regFOrmErrorMsg');
+const regFormUserName = document.querySelector('#regFormUserName');
+const regFormEmail = document.querySelector('#regFormEmail');
+const regFormPass = document.querySelector('#regFormPass');
+const regFormDOB = document.querySelector('#regFormDOB');
+
+// listening for a submit event on the form
+regForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if(regFormUserName.value === '' || regFormEmail.value === '' || regFormPass.value === '' || !regFormDOB.value) {
+        regFOrmErrorMsg.classList.add('error');
+        regFOrmErrorMsg.innerHTML = 'Please enter all fields';
+        setTimeout(() => regFOrmErrorMsg.remove(), 3000); // message disappears in 3 seconds
+    } else {
+        // clear fields
+        regFormUserName.value = '';
+        regFormEmail.value = '';
+        regFormPass.value = '';
+        regFormDOB.value = '';
+    }
+}
