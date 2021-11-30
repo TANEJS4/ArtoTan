a6f7202bc0270747c4925af7a46035f63fc171b8
Shivam <35265364+TANEJS4@users.noreply.github.com>
2021-11-30 02:49:17 -0500

    added DB support and php

---
+++ html/submission.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>ArtoTan - ObjectSubmission</title>
+
+        <!-- Encoding -->
+        <meta charset="UTF-8">
+
+        <!-- Mobile Specific Metas -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=0.9"/>
+
+        <!-- Local CSS -->
+        <link rel="stylesheet" href="../css/submission.css">
+
+        <!-- CSS - Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+        <!-- JS - Required for Bootstrap -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </head>
+
+    <body class="bg-secondary">
+
+        <?php include $_SERVER['DOCUMENT_ROOT'] .  '/html/header.html'?>
+
+        <main class="container-flex">
+            <form id="addLocForm" method="post" action="http://localhost/php/addObject.php">
+                <h1 id="formTitle">Add Location</h1>
+                <div class="addLocFormError"></div>
+                <div>
+                    <label for="addLocFormLocName">Location Name:</label>
+                    <input type="text" id="addLocFormLocName" name ="addLocFormLocName" required>
+                </div>
+                <div>
+                    <label for="addLocFormLocDesc">Location Description:</label>
+                    <input type="text" id="addLocFormLocDesc" name ="addLocFormLocDesc" required>
+                </div>
+                <div>
+                    <label for="addLocFormLocLat">Latitude (up to 6 decimal point):</label>
+                    <!-- Regex: Start with + or - and up to 6 decimals -->
+                    <input type="text" id="addLocFormLocLat" name="addLocFormLocLat" pattern="^(\+|-)?(?:90(?:(?:\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\.[0-9]{1,6})?))$" required>
+                </div>
+                <div>
+                    <label for="addLocFormLocLong">Longitude (up to 6 decimal point): </label>
+                    <!-- Regex: Start with + or - and up to 6 decimals -->
+                    <input type="text" id="addLocFormLocLong" name="addLocFormLocLong" pattern="^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$" required>
+                </div>
+                <div>
+                    <label for="addLocFormLocImg">Upload Location Image:</label>
+                    <input type="file" id="addLocFormLocImg" name="addLocFormLocImg">
+                </div>
+                <div>
+                    <button type="button" id="getCurrentLatLong" name="getCurrentLatLong">Get Current Location</button>
+                </div>
+                <input id="addLocFormButton" name="addLocFormButton" type="submit" value="Submit">
+            </form>
+        </main>
+
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/html/footer.html'?>
+
+    </body>
+    <!-- Local JS -->
+    <script src="../js/submission.js"></script>
+</html>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<title>ArtoTan – Homepage</title>
+
+		<!-- Encoding -->
+		<meta charset="UTF-8">
+
+		<!-- Mobile Specific Metas and encoding-->
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=0.9"/>
+
+	</head>
+
+	<body class="bg-secondary">
+		<?php include $_SERVER['DOCUMENT_ROOT'] .  '/html/header.html'?>
+	
+
+		<main>
+			<div class="container text-white">
+				<div class="row row-cols-1">
+					<!-- added p-lg-5 to add space on top (better job than margin for us) -->
+					<div class="col-md-auto p-lg-5 mx-auto my-2">
+						<div class="display-4">
+							Find coffee!
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="container">
+				<div class="row row-cols-1">
+					<div class="col-md-auto p-lg-5 mx-auto">
+						<div class="col-lg-auto">
+							<!-- Form to send it to database later on -->
+							<form id="searchBox" action="./html/result_sample.php" class="form-inline" method="post" >
+								<div class="input-group">
+									<input id="searchQuery" class="form-control form control" type="text" placeholder="Search" aria-label="Search for coffee shops">
+									<button class="btn btn-dark mx-2" type="submit" id="searchButton">Search</button>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+
+				<div class="row row-cols-1">
+					<div class="col-md-auto mx-auto">
+						<div class="col-lg-auto">
+							<div class="input-group">
+								<button class="btn btn-dark btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Find shops by
+								</button>
+								<div class="dropdown-menu">
+									<a class="dropdown-item" href="./html/result_sample.php">distance</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item" href="./html/result_sample.php">rating</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+
+	<?php include $_SERVER['DOCUMENT_ROOT'] . '/html/footer.html'?>
+	</body>
+	<!-- Local JS -->
+	<script src="./js/index.js"></script>
+</html>
